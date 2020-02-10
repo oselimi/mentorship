fae72fdfc0f535cb2a00710703d5ee73963d0aea
oselimi <60190920+oselimi@users.noreply.github.com>
2020-02-10 16:04:43 +0100

    Add files via upload

---
+++ 3_0.rb
@@ -0,0 +1,40 @@
+
+#Show just text within quotes
+	puts "I will now count my chickens:"
+
+#Show text within quotes and calculation
+	puts "Hens", 25 + 30 / 6
+
+#Show text and calculation
+	puts "Roosters", 100 - 25 * 3 % 4
+
+#Show just text within quotes
+	puts "Now I will count the eggs:"
+
+#Calculation
+	puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#Show text and numbers within quotes
+	puts "Is it true that 3 + 2 < 5 - 7?"
+
+#Output of this code is 'false' because 5 - 7 smaller of 3+5 
+	puts 3 + 2 < 5 - 7
+
+#Show text and numbers within quotes and calculation
+	puts "What is 3 + 2?", 3 + 2
+	puts "What is 5 - 7?", 5 - 7
+
+#Show text within quotes 
+	puts "Oh, that's why it's false."
+
+#Show text within quotes 
+	puts "How about some more."
+
+#Show text within quotes and resultate is boolean method 'true or false'
+	puts "Is it greater?", 5 > -2
+
+#Show text within quotes and resultate is boolean method 'true or false'
+	puts "Is it greater or equal?", 5 >= -2
+
+#Show text within quotes and resultate is boolean method 'true or false'
+	puts "Is it less or equal?", 5 <= -2
